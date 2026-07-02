eff8e3e0f6e6b03c932bae3fbd5120021df6fd21
baehenry45-collab <baehenry45@gmail.com>
2026-07-02 21:02:49 +0900

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("TteokAI Bot Running 🚀");
+  res.send("TteokAI is running 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
